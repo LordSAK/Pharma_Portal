35b3335886bac7546fd7f1c2a395ef515776419d
Saghir A. Khatri <saghir.alam@musewerx.com>
2014-04-03 14:07:12 +0500

    fixed query issues

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,7 +7,7 @@ class SearchController < ApplicationController
   	activity=params[:ActivityType] unless params[:ActivityType]=="All"
   	detail=params[:DetailType] unless params[:DetailType]=="All"
   	nature=params[:NatureOfPayment] unless params[:NatureOfPayment]=="All"
-  	@result=TransferOfValue.joins("inner join recipient_amounts on transfer_of_values.id=recipient_amounts.AmountID inner join users on recipient_amounts.RecipientID=users.id").where("(? is null or transfer_of_values.DateofPayment>= ? ) and (? is null or transfer_of_values.DateofPayment <= ?) and(? is null or users.userType=?) and (? is null or users.state=?) and (? is null or transfer_of_values.ActivityType=?) and (? is null or transfer_of_values.DetailType=?) and (? is null or NatureofPayment=?)",start,start,ends,ends,userType,userType,state,state,activity,activity,detail,detail,nature,nature)
+  	@result=TransferOfValue.joins("inner join 'recipient_amounts' on 'transfer_of_values'.'id'='recipient_amounts'.'AmountID' inner join 'users' on 'recipient_amounts'.'RecipientID'='users'.'id'").where("(? is nulll or 'transfer_of_values'.'DateofPayment' >= ? ) and (? is null or 'transfer_of_values'.'DateofPayment' <= ?) and (? is null or 'users'.'userType'=?) and (? is null or 'users'.'state'=?) and (? is null or 'transfer_of_values'.'ActivityType'=?) and (? is null or 'transfer_of_values'.'DetailType'=?) and (? is null or 'transfer_of_values'.'NatureofPayment'=?)",start,start,ends,ends,userType,userType,state,state,activity,activity,detail,detail,nature,nature)
   end
 
   def SearchDisputes
@@ -15,6 +15,6 @@ class SearchController < ApplicationController
 	ends=Date.strptime params[:End], '%m-%d-%Y'
 	status=params[:status] unless params[:status]=="All"
 	exceptions=params[:Exception] unless params[:Exception]=="All"
-	@dispute=Dispute.joins('inner join "transfer_of_values" on "disputes"."AmountID" = "transfer_of_values"."id"').where("(? is null or disputes.DisputeDate>= ? ) and (? is null or disputes.DisputeDate <= ?) and (? is null or disputes.Status = ? ) and (? is null or disputes.Description=?)",start,start,ends,ends,status,status,exceptions,exceptions)
+	@dispute=Dispute.joins("inner join 'transfer_of_values' on 'disputes'.'AmountID' = 'transfer_of_values'.'id'").where("(? is null or 'disputes'.'DisputeDate' >= ? ) and (? is null or 'disputes'.'DisputeDate' <= ?) and (? is null or 'disputes'.'Status' = ? ) and (? is null or 'disputes'.'Description'=?)",start,start,ends,ends,status,status,exceptions,exceptions)
   end
 end
